9bca4312300077f34a0072fbbf2bf195f36a310e
deftonjke <defton@mail.ru>
2021-08-07 16:27:39 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length !== 0) ? array.sort((a,b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length !== 0) ? array.sort((a,b) => b - a)[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length !== 0) ? array.reduce((sum, value) => sum + value) / array.length : 0;
 }
